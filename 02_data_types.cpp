4ca52a292d8d896742f324fe05f2dcb233a27303
Daksh <daksh.iitr.tech@gmail.com>
2026-06-29 17:53:07 +0530

    Create 02_data_types.cpp

---
+++ 02_data_types.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    //Working with Data Types
+    
+    int a = 4, b = 5;
+    float pi = 3.14;
+    char d = 'k';
+    bool x = true, y = false;
+    cout<<"Hey I am Daksh. \nHere, the value of a is "<<a<<". \nThe value of b is "<<b;
+    cout<<"\nThe value of pi is "<<pi<<".";
+    cout<<"\nThe value of d is "<<d<<".";
+    cout<<"\nx = "<<x<<"(true) \ny = "<<y<<"(false)";
+    return 0;
+}
