55c7b18236b1efcf9ea3558d716b48a7efafd244
Daksh <daksh.iitr.tech@gmail.com>
2026-06-29 18:47:10 +0530

    Create 10_while_loop.cpp

---
+++ 10_while_loop.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    
+    //While Loop
+    
+    /* syntax:
+       while(condition)
+       {
+       c++ statements
+       }
+    */
+       
+       //Suppose we have to print integers from 1 to 40
+    int i=1;
+    while(i<=40)
+    {
+        cout<<i<<endl;
+        i++;
+    }
+    return 0;
+}
