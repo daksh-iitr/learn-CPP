cedb3515de3f7a1e61cca9dfa9723457d2ef2c67
Daksh <daksh.iitr.tech@gmail.com>
2026-06-29 18:44:34 +0530

    Create 09_for_loop.cpp

---
+++ 09_for_loop.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    
+    /* Loops in C++ :
+       1. For Loops
+       2. While Loop
+       3. do-while Loop */
+       
+       /*For Loop:
+       
+        Syntax for For Loop:
+       for (initialization; condition; updation)
+       {
+           loop body (c++ code)
+       }
+       */
+       
+     //suppose we have to write integers from 1 to 100
+     
+     for (int i=1; i<101; i++)
+     {
+         cout<<i<<endl;
+     }
+     
+     //if we want this series upto infinite then we will write condition that will always be true
+       
+       
+    
+    
+    return 0;
+}
