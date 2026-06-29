a55d3b47e81d42b4aba82deea4ce0cd1baf9c448
Daksh <daksh.iitr.tech@gmail.com>
2026-06-29 18:54:42 +0530

    Create 12_multiplication_table.cpp

---
+++ 12_multiplication_table.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    //Write multiplication table of 6 using loops
+    
+    int i=1;
+    while(i<=10){
+        cout<<"6*"<<i<<"="<<6*i<<endl;
+        i++;
+    }
+    return 0;
+}
